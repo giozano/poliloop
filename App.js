8170e517c923536e1eb52f7da86f699c189d0a5e
Giovanni Zanocco <giovanni@zanocco.it>
2022-01-03 22:18:03 +0100

    initial commit

---
+++ App.js
@@ -0,0 +1,10 @@
+import './App.css';
+import Start from './pages/Start/Start';
+
+export default function App() {
+  return (
+    <div className="App">
+      <Start></Start>
+    </div>
+  );
+}
---
+++ pages/Start/Start.js
@@ -0,0 +1,9 @@
+import './Start.css';
+
+export default function Start() {
+  return (
+    <div className="Start">
+        <button>START</button>
+    </div>
+  );
+}
